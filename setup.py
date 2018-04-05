f9a6f395845545fd61dd179d86d6995eb7113c56
Shuo Han <shan50@jhu.edu>
2018-04-05 17:42:23 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plotting-utils',
+      version='0.0.1',
+      description='Utility for plotting',
+      author='Shuo Han',
+      author_email='shan50@jhu.edu',
+      packages=['plotting_utils']
+     )
