e70368d835bfe71abd587230a5e64bb6a8f8545b
Shuo Han <shan50@jhu.edu>
2018-04-05 17:45:03 -0400

    Correct typos

--- plotting_utils/__init__.py
+++ plotting_utils/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from .geometry import vectorize, Line, Plane
-from .plotting_funs import set_equal_3d_axis, plot_line, plot_plane
-from .plotting_funs import plot_3d_image, get_colors, get_marker_styles
+from .plotting_funcs import set_equal_3d_axis, plot_line, plot_plane
+from .plotting_funcs import plot_3d_image, get_colors, get_marker_styles
